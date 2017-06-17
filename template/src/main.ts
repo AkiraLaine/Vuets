0bd274428309d6e39f8cc9a7b7d56baf17e25d4f
Akira Laine <akira_laine@outlook.com>
2017-06-17 15:27:23 +1100

    vue-cli axios option

--- template/src/main.ts
+++ template/src/main.ts
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App'
+{{#plugins.axios}}
+import axios from 'axios'
+{{/plugins.axios}}
 {{#plugins.vue-router}}
 import router from './router'
 {{/plugins.vue-router}}
+
+{{#plugins.axios}}
+Vue.prototype.$http = axios
+{{/plugins.axios}}
 
 /* eslint-disable no-new */
 new Vue({
